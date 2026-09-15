7d7e11a769a0e997b14f7a3ca15c3576f6930521
Judith Martínez <aguimtz.2003@gmail.com>
2026-09-15 01:12:25 -0600

    Edit: data

--- src/data/people.js
+++ src/data/people.js
@@ -8,7 +8,7 @@ export const invitadosCeremonia = {
   R6tU40: "Ingeniero Jovis",
   S3vW51: "Ingeniero Yisus",
   T1xY62: "Ingeniero Gask",
-  U4zA73: "Zulma",
+  U4zA73: "Maestra Zulma",
   V7bB84: "Rubén",
   Q4xL82: "Yessenia y Teodoro",
 };
